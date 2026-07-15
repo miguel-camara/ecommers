18c01590f96fc3b4a81c567172916cf946e4df40
Miguel Ordonez Camara <miguelordonezcamara@hotmail.com>
2026-07-14 21:24:48 -0600

    proyecto inicial

---
+++ src/test/java/com/migueldev/ecommers/EcommersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.migueldev.ecommers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
